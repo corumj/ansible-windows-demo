631fc6e2e8857203a4a9e9b751756a8bfd7b666c
Jerry Corum <corum@me.com>
2021-01-25 11:03:26 -0700

    modify to take care of git pull too

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pushd "$( dirname "${BASH_SOURCE[0]}" )"
+git reset --hard
+git pull 
 ansible-galaxy collection install -r collections/requirements.yml
 ansible-playbook demo_setup.yml 
 popd
